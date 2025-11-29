f09767120710878cb85aa276cc97e27a3adc8bbf
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2025-11-29 16:59:42 +0000

    Tweaks

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ globalThis.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
 // globalThis.MbglLogging = {
 //   level: 'error', // only show critical errors
 // };
+
+import { Logger } from "@maplibre/maplibre-react-native";
+
+// Hide tile-cancel spam
+Logger.setLogCallback(log => {
+    const msg = log.message || "";
+
+    if (
+        msg.includes("Request failed due to a permanent error: Canceled") ||
+        msg.includes("Socket Closed")
+    ) {
+        return true;
+    }
+    return false;
+})
+
 AppRegistry.registerComponent(appName, () => App);
